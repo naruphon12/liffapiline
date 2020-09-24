0b9aacc4da78a2dc7c0fd9ffc759d509c0385c7f
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-09-24 11:01:40 +0700

    Add files via upload

---
+++ callservice.js
@@ -0,0 +1,82 @@
+    const express = require('express')
+    const bodyParser = require('body-parser')
+    const request = require('request')
+    var RequestService = require("request");
+    const app = express()
+    const soapRequest = require('easy-soap-request')
+    const fs = require('fs')
+
+   
+    async function Registerline() {
+        var phoneNo = document.getElementById('phone').value;
+        const profile = await liff.getProfile()
+        let num = ''
+        var Lineid = ''
+        var userProfiles = ''
+        var username = ''
+        var lineurl = ''
+        var phone=''
+        num = num + liff.getContext().type
+        username=profile.displayName
+        alert(username +" &&&& "+ num)
+  
+        if (liff.getContext().type == "external") {
+          
+          document.getElementById("ID").append(liff.getContext().utouId)
+          Lineid=liff.getContext().utouId
+          lineurl =  liff.getDecodedIDToken().email
+        }else if(liff.getContext().type  =="group"){
+        
+          document.getElementById("ID").append(liff.getContext().groupId)
+          Lineid=liff.getContext().groupId
+          lineurl =  liff.getDecodedIDToken().email
+        }else if(liff.getContext().type == "room"){
+         
+          document.getElementById("ID").append(liff.getContext().roomId)
+          Lineid=liff.getContext().roomId
+          lineurl =  liff.getDecodedIDToken().email
+        }else if(liff.getContext().type == "utou"){
+          
+          document.getElementById("ID").append(liff.getContext().utouId)
+          Lineid=liff.getContext().utouId
+          lineurl =  liff.getDecodedIDToken().email
+        }
+        phone=document.getElementById("phone").value
+        alert(phone + " ----"+ Lineid)
+        alert(lineurl + " email")
+        try {
+          alert("1")
+          RequestService.post({
+            "headers": { "content-type": "application/json" },
+            "url": "https://feeddev-line.cpf.co.th/FeedRegisterLineBot/api/Register",
+            "body": JSON.stringify({
+              "User_ID": Lineid,
+              "Phone_No": phone,
+              "Email": lineurl,
+              "Nameline": username
+            })
+            
+          }, (error, response, body) => {
+            if (error) {
+              console.dir(error);
+              alert("error"+ error)
+  
+              return error;
+            } else {
+              console.dir(JSON.parse(body));
+              num = num + JSON.parse(body);
+              alert("error"+ num)
+              sendMsg()
+            }
+          });
+        } catch (e) {
+          console.log(e);
+          err = e
+          alert(err)
+          num = num + "เข้า" + err
+        }
+        finally {
+          num = "หมายเลข : " + phonenumber + " "
+          alert(num)
+        }
+      }
